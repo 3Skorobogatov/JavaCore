c5c03d3e460156f49a1daaf83c91011a3f9fffe4
Skorobogatov Sergei <ssskorobogatov@ya.ru>
2023-12-08 16:26:28 +0400

    change package and classes

---
+++ src/courseAT/check.java
@@ -0,0 +1,7 @@
+package courseAT;
+
+public class check {
+    public static void main(String[] args) {
+        System.out.println(lesson1.abs(-12));
+    }
+}
--- src/lesson1.java
+++
@@ -1,8 +0,0 @@
-public class lesson1 {
-    public static void main(String[] args) {
-        int x = -13;
-        int x1;
-        x1 = Math.abs(x);
-        System.out.println("Модуль числа x = "+x1);
-    }
-}
