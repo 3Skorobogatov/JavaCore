c49b2aa992d47db350527dd284039e291b71c856
Skorobogatov Sergei <ssskorobogatov@ya.ru>
2023-12-09 00:22:23 +0400

    add lesson7

--- src/courseAT/check.java
+++ src/courseAT/check.java
@@ -2,6 +2,6 @@ package courseAT;
 
 public class check {
     public static void main(String[] args) {
-        System.out.println(lesson6.sum3(2,1,1));
+        System.out.println(lesson7.sum2(22,-5));
     }
 }
