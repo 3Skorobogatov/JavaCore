b0b07f69913956e30d033481381178b4630b6965
Skorobogatov Sergei <ssskorobogatov@ya.ru>
2023-12-08 12:38:41 +0400

    Initial commit

---
+++ src/absX.java
@@ -0,0 +1,8 @@
+public class absX {
+    public static void main(String[] args) {
+        int x = -13;
+        int x1;
+        x1 = Math.abs(x);
+        System.out.println("Модуль числа x = "+x1);
+    }
+}
