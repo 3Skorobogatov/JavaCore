d3dc16ceda34d1f4f841c91156ea363aa292ade4
Skorobogatov Sergei <ssskorobogatov@ya.ru>
2023-12-09 17:26:40 +0400

    update

--- src/courseAT/check.java
+++ src/courseAT/check.java
@@ -1,11 +1,11 @@
 package courseAT;
 
 import courseAT.conditions_IF.lesson11;
-
+import courseAT.conditions_IF.lesson12;
 import courseAT.conditions_IF.lesson4;
 
 public class check {
     public static void main(String[] args) {
-        System.out.println(lesson11.day(3));
+        System.out.println(lesson12.printDays("воскресенье"));
     }
 }
--- src/courseAT/conditions_IF/lesson12.java
+++ src/courseAT/conditions_IF/lesson12.java
@@ -1,24 +1,18 @@
 package courseAT.conditions_IF;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class lesson12 {
-    public static void printDays(String x){
-        Scanner scanner = new Scanner(System.in);
+    public static String printDays(String x){
         String[] days = {"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"};
         String incorrect = "это не день недели";
         switch (x){
-            case "понедельник" : System.out.println(days[0]+", "+ days[1] +", " + days[2] + ", " + days[3] + ", " + days[4] + ", "+days[5] + ", " + days[6]); break;
-            case "вторник" : System.out.println(days[1]+", "+days[2]+", "+days[3]+", "+days[4]+", "+days[5]+", "+days[6]); break;
-            case "среда" : System.out.println(days[2]+", "+days[3]+", "+days[4]+", "+days[5]+", "+days[6]); break;
-            case "четверг" : System.out.println(days[3]+", "+days[4]+", "+days[5]+", "+days[6]);break;
-            case "пятница" : System.out.println(days[4]+", "+days[5]+", "+days[6]);break;
-            case "суббота" : System.out.println(days[5]+", "+days[6]); break;
-            case "воскресенье" : System.out.println(days[6]); break;
+            case "понедельник" : return (days[0]+", "+ days[1] +", " + days[2] + ", " + days[3] + ", " + days[4] + ", "+days[5] + ", " + days[6]);
+            case "вторник" : return (days[1]+", "+days[2]+", "+days[3]+", "+days[4]+", "+days[5]+", "+days[6]);
+            case "среда" : return (days[2]+", "+days[3]+", "+days[4]+", "+days[5]+", "+days[6]);
+            case "четверг" : return (days[3]+", "+days[4]+", "+days[5]+", "+days[6]);
+            case "пятница" : return (days[4]+", "+days[5]+", "+days[6]);
+            case "суббота" : return (days[5]+", "+days[6]);
+            case "воскресенье" : return (days[6]);
         }
-
+    return incorrect;
     }
 
 }
